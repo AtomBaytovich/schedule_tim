49e3b5c3e7c7cb6cc344a398d16a6a22ff595bc7
Atom Baytovich <atombaytovich@gmail.com>
2024-10-14 14:20:59 +0300

    style

--- controllers/commands.js
+++ controllers/commands.js
@@ -1,9 +1,9 @@
 const raspJson = require('../rasp.json');
-const moment = require('moment-timezone')
+const moment = require('moment-timezone');
 const { dateRaspButton, menuButton } = require('./buttons');
 const { getNumberDay, smileFACE, getTimePar } = require('../tools/helpers');
-moment.locale("ru")
-moment.tz.setDefault('Europe/Moscow')
+moment.locale("ru");
+moment.tz.setDefault('Europe/Moscow');
 
 const start = (ctx) => {
     return ctx.reply(`
@@ -33,8 +33,6 @@ function updateDateInfo(date = moment()) {
     return { dateBack, dateNext, dayWeek, chetOrNechet };
 }
 
-
-
 const rasp = (command, date) => {
     try {
         let { dateBack, dateNext, dayWeek, chetOrNechet } = updateDateInfo();
@@ -43,30 +41,28 @@ const rasp = (command, date) => {
             ({ dateBack, dateNext, dayWeek, chetOrNechet } = updateDateInfo(moment(date)));
         }
 
-        let dayWeeekNumber = getNumberDay(dayWeek)
-        const dataNow = raspJson[dayWeeekNumber][dayWeek]
+        let dayWeeekNumber = getNumberDay(dayWeek);
+        const dataNow = raspJson[dayWeeekNumber][dayWeek];
 
         let text = ``;
-        text += `Неделя: ${chetOrNechet}, ${moment(date).format('DD.MM.YYYY')}, ${dayWeek}\n`
+        text += `Неделя: ${chetOrNechet}, ${moment(date).format('DD.MM.YYYY')}, ${dayWeek}\n`;
 
         for (let i = 0; i < dataNow.length; i++) {
-            
             if (dataNow[i].length > 1) {
-                // console.log(dataNow[i])
-                if (chetOrNechet == 'нечётная') {
-                    if (dataNow[i][0].length < 2) continue
-                    text += `\n\n${smileFACE(i + 1)}. <b>${getTimePar(i + 1)}</b> | <code>${dataNow[i][0]}</code>`
+                if (chetOrNechet === 'нечётная') {
+                    if (dataNow[i][0].length < 2) continue;
+                    text += `\n\n${smileFACE(i + 1)}. <b>${getTimePar(i + 1)}</b> | <code>${dataNow[i][0]}</code>`;
                 } else {
-                    if (dataNow[i][1].length < 2) continue
-                    text += `\n\n${smileFACE(i + 1)}. <b>${getTimePar(i + 1)}</b> | <code>${dataNow[i][1]}</code>`
+                    if (dataNow[i][1].length < 2) continue;
+                    text += `\n\n${smileFACE(i + 1)}. <b>${getTimePar(i + 1)}</b> | <code>${dataNow[i][1]}</code>`;
                 }
             } else {
                 if (dataNow[i][0].length < 2) {
-                    text +=  `\n\n${smileFACE(i + 1)}. <b>${getTimePar(i + 1)}</b> | <code>Пары нет. Окно</code>`
-                    continue
+                    text += `\n\n${smileFACE(i + 1)}. <b>${getTimePar(i + 1)}</b> | <code>Пары нет. Окно</code>`;
+                    continue;
                 }
-                text += `\n\n${smileFACE(i + 1)}. <b>${getTimePar(i + 1)}</b> | <code>${dataNow[i][0]}</code>`
-            } 
+                text += `\n\n${smileFACE(i + 1)}. <b>${getTimePar(i + 1)}</b> | <code>${dataNow[i][0]}</code>`;
+            }
         }
 
         const isNow = moment(date).isSame(moment(), 'day');
@@ -78,36 +74,41 @@ const rasp = (command, date) => {
         }
 
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
-
-const raspMessage = (ctx, isDel) => {
+const raspMessage = (ctx, isEdit) => {
     try {
-        let { text, dateBack, dateNext, isNow } = rasp(ctx)
-        if (isDel) ctx.deleteMessage()
+        let { text, dateBack, dateNext, isNow } = rasp(ctx);
 
-        ctx.replyWithHTML(text, {
-            ...dateRaspButton({ dateBack, dateNext, isNow })
-        });
+        if (isEdit) {
+            ctx.editMessageText(text, {
+                parse_mode: 'HTML',
+                ...dateRaspButton({ dateBack, dateNext, isNow })
+            }).catch((error) => console.log(error));
+        } else {
+            ctx.replyWithHTML(text, {
+                ...dateRaspButton({ dateBack, dateNext, isNow })
+            });
+        }
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
 const raspMovementMessage = (ctx) => {
     try {
-        const dateCallback = ctx.match["input"].split(" ")[1]
+        const dateCallback = ctx.match["input"].split(" ")[1];
 
-        let { text, dateBack, dateNext, isNow } = rasp("движение", moment(dateCallback, "DD MM YYYY"))
+        let { text, dateBack, dateNext, isNow } = rasp("движение", moment(dateCallback, "DD MM YYYY"));
 
-        ctx.deleteMessage()
-        ctx.replyWithHTML(text, {
+        ctx.editMessageText(text, {
+            parse_mode: 'HTML',
             ...dateRaspButton({ dateBack, dateNext, isNow })
-        });
+        }).catch((error) => console.log(error));
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
@@ -118,12 +119,14 @@ const help = (ctx) => {
 /rasp - покажет тебе расписание на сегодняшний день, кнопки ⬅️➡️ помогут посмотреть что было вчера или будет завтра
 Если появляется ♦️, то можно вернуться на сегодняшний день по расписанию
 
+Спасибо <a href="https://t.me/s0ft1">Глебасу</a>! Вместе оплачиваем сервак ❤️ 
 
 `, {
         ...menuButton,
         disable_web_page_preview: true
-    })
+    });
 }
+
 ///calend - покажет тебе календарное расписание занятий
 module.exports = {
     start,
